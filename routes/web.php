19011477bf05986ccec82064adc6ffe12c02a690
Fakare Alam <fakarealamfit@gmail.com>
2026-06-25 01:17:40 +0530

    web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('products.index');
+    return view('welcome');
 });
 
 Route::resource('products', ProductController::class);
